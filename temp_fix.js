99e8feef43b850e41355888664678049c56097af
cuberJY <zhaojunyi517@qq.com>
2026-07-09 19:19:19 +0800

    Initial commit: HarmonyOS LLM Chat Categorize

---
+++ temp_fix.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs');
+const path = 'd:/ZJY/Code/ChatCategorize/entry/src/main/ets/pages/Index.ets';
+let c = fs.readFileSync(path, 'utf8');
+// Fix: 3 single quotes -> 2 single quotes for empty string
+c = c.replace(/new Conversation\(0, '''''\)/g, 'new Conversation(0, \x27\x27)');
+c = c.replace(/\.trim\(\) === '''''/g, '.trim() === \x27\x27');
+c = c.replace(/this\.inputText = '''''/g, 'this.inputText = \x27\x27');
+fs.writeFileSync(path, c, 'utf8');
+console.log('done');
